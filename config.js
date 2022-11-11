3b5caf152d198ad88947c3ad0f36e01bc4d5cf0e
LongBinU <quanglongmai@gmail.com>
2022-11-12 00:32:32 +0700

    a

---
+++ config.js
@@ -0,0 +1,22 @@
+﻿
+var config = {
+  
+lines: [
+        "Hello Ú",
+        " nhanh ha :3 chua chi 5 tháng rùi",
+        " khong thang nao la khong co ki niem",
+        " co nhung luc khong hieu y nhau gian nhau",
+        "nhung luc noi chuyen dua vui vu vo voi nhau ",
+        "bat cu ki niem cung em doi voi anh deu la qui gia",
+        "Anh mong khong chi 5 thang ma ca sau deu co em",
+     " Happy 5 month anniversary", " LOVE YOU <3",],
+    // 相爱的时间 记住格式不能写错了, 非常重要
+    // 格式一定要和下面这个时间格式一样
+    // 分别是 年-月-日T时时:分分:秒秒
+    //      温馨警告:
+    //          1.时分秒都必须是俩个数字  比如 00:00:00
+    //          2.T不能丢了
+    date: "2022-06-12T12:00:00",
+    // 双方的名字
+    names: ["NGUYEN THI THANH", "MAI QUANG LONG"]
+}
