25e3e5c2a4d67dc39aaa459dd374508175b8e639
LongBinU <quanglongmai@gmail.com>
2022-11-12 09:35:23 +0700

    LONG

--- config.js
+++ config.js
@@ -1,16 +1,28 @@
 ﻿
 var config = {
   
-lines: [
-        "Hello Ú",
-        "Nhanh ha :3 chua chi 5 tháng rùi",
-        "Khong thang nao la khong co ki niem",
-        "Co nhung luc khong hieu y nhau gian nhau",
-        "Nhung luc noi chuyen dua vui vu vo voi nhau ",
-        "Bat cu ki niem nao cung em doi voi anh deu la qui gia",
-      "Mong rang hai ta se luon ben nhau",    "Tao them nhieu ki niem nua cung nhau",
-     "Happy 5 month anniversary", "LOVE YOU <3",],
-    // 相爱的时间 记住格式不能写错了, 非常重要
+lines: 
+[
+        
+"Hello Ú",
+        
+"Nhanh ha :3 chua chi 5 tháng rùi",
+        
+"Khong thang nao la khong co ki niem",
+        
+"Co nhung luc khong hieu y nhau gian nhau",
+        
+"Nhung luc noi chuyen dua vui vu vo voi nhau ",
+        
+"Bat cu ki niem nao cung em doi voi anh deu la qui gia",
+      
+"Mong rang hai ta se luon ben nhau",    
+"Tao them nhieu ki niem nua cung nhau",
+     
+"Happy 5 month anniversary", 
+"LOVE YOU <3",],
+    
+// 相爱的时间 记住格式不能写错了, 非常重要
     // 格式一定要和下面这个时间格式一样
     // 分别是 年-月-日T时时:分分:秒秒
     //      温馨警告:
